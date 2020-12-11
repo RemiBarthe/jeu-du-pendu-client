6f64d6abf3245c5139da6506552f85e4ead4b010
RemiBarthe <barthe.rem@gmail.com>
2020-12-11 23:38:05 +0100

    Add component gameRoom

--- src/components/GameRoom.vue
+++ src/components/GameRoom.vue
@@ -1,3 +1,33 @@
+<template>
+  <v-container>
+    <div class="list-letters">
+      <v-card outlined>
+        <p>C</p>
+      </v-card>
+
+      <v-card outlined>
+        <p>H</p>
+      </v-card>
+
+      <v-card outlined>
+        <p>A</p>
+      </v-card>
+
+      <v-card outlined>
+        <p>I</p>
+      </v-card>
+
+      <v-card outlined>
+        <p>S</p>
+      </v-card>
+
+      <v-card outlined>
+        <p>E</p>
+      </v-card>
+    </div>
+  </v-container>
+</template>
+
 <script>
 export default {
   name: "GameRoom",
@@ -5,3 +35,30 @@ export default {
   data: () => ({})
 };
 </script>
+
+<style scoped>
+.container {
+  height: 100vh;
+}
+
+.list-letters {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.list-letters .v-card {
+  margin: 5px;
+  width: 80px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.list-letters p {
+  font-size: 50px;
+  text-align: center;
+}
+</style>
