18b0db1f4afd3d2291ddfae1196ecea672613467
rgl243 <64373934+rgl243@users.noreply.github.com>
2020-12-13 16:18:59 +0100

    add modal letter

--- src/components/GameRoom.vue
+++ src/components/GameRoom.vue
@@ -25,14 +25,62 @@
         <p>E</p>
       </v-card>
     </div>
+    <v-row justify="center">
+      <v-dialog v-model="selectedWord" width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            @click="dialog = true"
+          >
+            test
+          </v-btn>
+        </template>
+        <div align="center">
+          <v-card width="370">
+            <v-card-title>
+              Cette lettre fait partie de votre mot ?
+            </v-card-title>
+            <h1 class="letter">H</h1>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <div class="align-center">
+                <v-btn class="validateBtn" rounded outlined @click="Validate()">
+                  Oui
+                </v-btn>
+                <v-btn class="declineBtn" rounded outlined @click="Decline()">
+                  Non
+                </v-btn>
+              </div>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-dialog>
+    </v-row>
   </v-container>
 </template>
+
 
 <script>
 export default {
   name: "GameRoom",
 
-  data: () => ({})
+  data: () => ({
+    selectedWord: true
+  }),
+
+  methods: {
+    Validate() {
+      console.log("oui");
+      this.selectedWord = false;
+    },
+    Decline() {
+      console.log("Non");
+      this.selectedWord = false;
+    }
+  }
 };
 </script>
 
@@ -47,7 +95,15 @@ export default {
   align-items: center;
   justify-content: center;
 }
-
+.letter {
+  margin: 30px;
+}
+.declineBtn {
+  margin-right: 100px;
+}
+.validateBtn {
+  margin-right: 25px;
+}
 .list-letters .v-card {
   margin: 5px;
   width: 80px;
