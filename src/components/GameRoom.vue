0a9da9ebae5b4b293df096ca33bf85066aa36246
rgl243 <64373934+rgl243@users.noreply.github.com>
2020-12-23 01:40:43 +0100

    Add return to zero
    edit the AddToLettersPositions function to add the return to 0

--- src/components/GameRoom.vue
+++ src/components/GameRoom.vue
@@ -129,6 +129,7 @@ export default {
       }
     },
     addToLettersPositions() {
+      let count = 0;
       this.word.forEach((letter) => {
         if (letter.value === this.letterPossible) {
           if (this.letters == "") {
@@ -138,8 +139,18 @@ export default {
             this.letters += "," + this.letterPossible;
             this.positions += "," + letter.position;
           }
+          count++;
         }
       });
+      if (!count) {
+        if (this.letters == "") {
+          this.letters += this.letterPossible;
+          this.positions += 0;
+        } else {
+          this.letters += "," + this.letterPossible;
+          this.positions += "," + 0;
+        }
+      }
     },
     getWords() {
       this.addToLettersPositions();
